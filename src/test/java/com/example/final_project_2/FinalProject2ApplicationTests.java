925fafc4d816f55c8a226e5629b05583f362221e
sahargholami1992 <sahar_gholami03@gmail.com>
2024-01-23 12:59:15 +0330

    Initial commit

---
+++ src/test/java/com/example/final_project_2/FinalProject2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.final_project_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinalProject2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
