e85adc8f592784605c56939afa54e7747e218ec9
sahargholami1992 <sahar_gholami03@gmail.com>
2024-01-27 23:36:10 +0330

    commit oder service and repository

--- src/main/java/com/example/final_project_2/repository/OrderRepository.java
+++ src/main/java/com/example/final_project_2/repository/OrderRepository.java
@@ -1,8 +1,24 @@
 package com.example.final_project_2.repository;
 
 
+import com.example.final_project_2.entity.Customer;
+import com.example.final_project_2.entity.Expert;
+import com.example.final_project_2.entity.Offer;
 import com.example.final_project_2.entity.Order;
+import jakarta.persistence.Entity;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Collection;
+import java.util.List;
 
 public interface OrderRepository extends JpaRepository<Order,Integer> {
+   @Query("SELECT o FROM Order o JOIN o.subService s JOIN s.experts e WHERE e = :expert")
+   List<Order> findOrderByForExpert(@Param("expert") Expert expert);
+
+
 }
+
+
+
--- src/main/java/com/example/final_project_2/service/OrderService.java
+++ src/main/java/com/example/final_project_2/service/OrderService.java
@@ -2,9 +2,20 @@ package com.example.final_project_2.service;
 
 
 import com.example.final_project_2.entity.Customer;
+import com.example.final_project_2.entity.Expert;
+import com.example.final_project_2.entity.Order;
 import com.example.final_project_2.entity.SubService;
+import com.example.final_project_2.entity.enumaration.StatusOrder;
 import com.example.final_project_2.service.dto.OrderDto;
 
+import java.util.Collection;
+
 public interface OrderService  {
-    void registerOrder(OrderDto dto, Customer customer, SubService subService);
+    Order registerOrder(OrderDto dto, Customer customer, SubService subService);
+    Collection<Order> getPendingOrdersForExpert(Expert expert);
+
+    void UpdateStatus(Order order);
+    void changeOrderStatus(Order order, StatusOrder statusOrder);
+    Collection<Order> findAll();
+
 }
